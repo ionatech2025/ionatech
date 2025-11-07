ec70c8d7816b2b7c056f6b693578ea830a5285be
Allanella <allanbaliddawa@gmail.com>
2025-11-07 10:23:32 +0300

    � Removed nested git repo and added clean project files

---
+++ iona-tech/src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Tech from './Components/Tech/Tech'
+import Programs from './Components/Programs/Programs'
+import MyTitle from './Components/MyTitle/MyTitle'
+import About from './Components/About/About'
+import Products from './Components/Products/Products'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contacts from './Components/Contacts/Contacts'
+import Footer from './Components/Footer/Footer'
+import ClientTestimonials from './Components/ClientTestimonials/ClientTestimonials'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      
+      {/* Hero Section */}
+      <section id="hero">
+        <Tech />
+      </section>
+
+      {/* Services Section */}
+      <section id="descrip">
+        <MyTitle />
+      </section>
+
+      {/* Programs Section */}
+      <section id="programs">
+        <Programs />
+      </section>
+
+      {/* About Section */}
+      <section id="about">
+        <About />
+      </section>
+
+      {/* Products Section */}
+      <section id="MyProducts">
+        <Products />
+      </section>
+
+      {/* Team Section */}
+      <section id="testimonials-section">
+        <Testimonials />
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact_us">
+        <Contacts />
+      </section>
+
+      {/*ClientTestimonials Section*/}
+      <ClientTestimonials/>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
